25556c1dcc1ac79925f28aea09da74963c8e5ea2
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-19 06:48:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,10 @@
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  array = [ "hello",  "world"]
 end
+  print count_strings
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array = [ "", "Hello", 4, [], "", "" ]
+  ount_empty_strings(array)
 end
